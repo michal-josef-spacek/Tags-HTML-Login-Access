b4418a0260f3ad2890adf0e1cddb8dcaf8c5a969
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-04-29 13:24:00 +0200

    Prepare for 0.10 version

--- Access.pm
+++ Access.pm
@@ -13,7 +13,7 @@ use Tags::HTML::Messages;
 
 Readonly::Array our @FORM_METHODS => qw(post get);
 
-our $VERSION = 0.09;
+our $VERSION = 0.10;
 
 # Constructor.
 sub new {
@@ -593,6 +593,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.09
+0.10
 
 =cut
